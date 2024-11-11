af7701b697f06eb307b538c9ffd7029a6e9c41ad
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-11-11 09:55:58 +0100

    Seed database with initial todos

--- config/index.js
+++ config/index.js
@@ -1,10 +1,7 @@
 var configValues = require('./config');
 
 module.exports = {
-
     getDbConnectionString: function() {
-        return 'mongodb+srv://' + configValues.uname + ':' + configValues.pwd + 
-            '@cluster0.fm3if.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+      return `mongodb+srv://${configValues.uname}:${configValues.pwd}@cluster0.fm3if.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
     }
-
-}
+  }
